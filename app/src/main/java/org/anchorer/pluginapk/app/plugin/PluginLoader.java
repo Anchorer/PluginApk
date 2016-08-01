feff5c6df186d2d6dffd577f19b4aac635fdd471
Anchorer <anchorer.du@dingtone.me>
2016-08-01 10:12:12 +0800

    简化代码

--- app/src/main/java/org/anchorer/pluginapk/app/plugin/PluginLoader.java
+++ app/src/main/java/org/anchorer/pluginapk/app/plugin/PluginLoader.java
@@ -3,7 +3,6 @@ package org.anchorer.pluginapk.app.plugin;
 import android.util.Log;
 
 import org.anchorer.pluginapk.app.TestApplication;
-import org.anchorer.pluginapk.app.common.CommonUtils;
 import org.anchorer.pluginapk.app.common.Const;
 
 import java.io.BufferedInputStream;
@@ -12,28 +11,25 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Field;
 
-import dalvik.system.BaseDexClassLoader;
 import dalvik.system.DexClassLoader;
-import dalvik.system.PathClassLoader;
 
 public class PluginLoader {
 
-	private static final int LOAD_PLUGIN_APK_STATE_INIT = 0;
-	private static final int LOAD_PLUGIN_APK_STATE_LOADED = 2;
-	private int mLoadPluginApkState = LOAD_PLUGIN_APK_STATE_INIT;
-
 	private DexClassLoader mDexClassLoader;
 
+	/**
+	 * 加载插件APK
+	 * @param pluginName	插件APK的名称
+     */
 	public void loadPluginApk(String pluginName) {
-		DexClassLoader classLoader = createDexClassLoader(pluginName);
-		if (classLoader != null) {
-			inject(classLoader);
-		}
-		mLoadPluginApkState = LOAD_PLUGIN_APK_STATE_LOADED;
+		mDexClassLoader = createDexClassLoader(pluginName);
 	}
 
+	/**
+	 * 加载指定名称的类
+	 * @param className	类名(包含包名)
+     */
 	public Object newInstance(String className) {
 		if (mDexClassLoader == null) {
 			return null;
@@ -127,63 +123,5 @@ public class PluginLoader {
 			classLoader = new DexClassLoader(apkPath, dexOutputDirPath, null, cl);
 		} catch(Throwable e) {}
 		return classLoader;
-	}
-
-	public boolean inject(DexClassLoader dexClassLoader) {
-	    try {
-	        Class.forName("dalvik.system.BaseDexClassLoader");
-	    } catch (ClassNotFoundException e) {}
-
-		PathClassLoader pathClassLoader = (PathClassLoader)TestApplication.getInstance().getClassLoader();
-		Log.d(Const.LOG, "path class loader class = " + PathClassLoader.class.toString());
-		try {
-			Object dexElements = CommonUtils.combineArray(getDexElements(getPathList(pathClassLoader)), getDexElements(getPathList(dexClassLoader)));
-			Object pathList = getPathList(pathClassLoader);
-			setField(pathList, pathList.getClass(), "dexElements", dexElements);
-			Log.d(Const.LOG, "inject pahtClassLoader = " + pathClassLoader);
-			mDexClassLoader = dexClassLoader;
-			return true;
-		} catch (Throwable e) {
-			return false;
-		}
-	}
-
-	/**
-	 * The BaseDexClassLoader definition
-	 *  https://android.googlesource.com/platform/libcore-snapshot/+/ics-mr1/dalvik/src/main/java/dalvik/system/BaseDexClassLoader.java
-	 * @param pathClassLoader
-	 * @return
-	 * @throws Exception
-	 */
-	private Object getPathList(Object pathClassLoader) throws Exception {
-		Field pathField = BaseDexClassLoader.class.getDeclaredField("pathList");
-		pathField.setAccessible(true);
-		Object pathListObj = pathField.get(pathClassLoader);
-		Log.d(Const.LOG, " pathListObj = " + pathListObj);
-		return pathListObj;
-	}
-	
-	private Object getDexElements(Object pathListObj) throws Exception {
-		Log.d(Const.LOG, "getDexElements pathListObj = " + pathListObj);
-
-		Field dexElementsField = pathListObj.getClass().getDeclaredField("dexElements");
-		dexElementsField.setAccessible(true);
-		Object dexElements = dexElementsField.get(pathListObj);
-		
-		Log.d(Const.LOG, " dexElements = " + dexElements);
-		return dexElements;
-	}
-	
-	private void setField(Object obj, Class<? extends Object> clazz, String filedName, Object value) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-		Field field = clazz.getDeclaredField(filedName);
-		field.setAccessible(true);
-		field.set(obj, value);
-	}
-
-	/**
-	 * 检查插件APK是否已经load成功
-     */
-	public static int getLoadPluginApkStateLoaded() {
-		return LOAD_PLUGIN_APK_STATE_LOADED;
 	}
 }
